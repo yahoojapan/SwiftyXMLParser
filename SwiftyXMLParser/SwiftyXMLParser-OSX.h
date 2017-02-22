b84acd0aa8e55c9f2bcae8b565ffc399527d2536
bono.yusuke <bono.uzkit@gmail.com>
2017-02-22 15:31:48 +0900

    Add Mac OSX target

---
+++ SwiftyXMLParser/SwiftyXMLParser-OSX.h
@@ -0,0 +1,19 @@
+//
+//  SwiftyXML MacOS.h
+//  SwiftyXML MacOS
+//
+//  Created by bono.yusuke on 2017/02/22.
+//  Copyright © 2017年 Yahoo! Japan. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for SwiftyXML MacOS.
+FOUNDATION_EXPORT double SwiftyXMLParserVersionNumber;
+
+//! Project version string for SwiftyXML MacOS.
+FOUNDATION_EXPORT const unsigned char SwiftyXMLParserVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SwiftyXML_MacOS/PublicHeader.h>
+
+
